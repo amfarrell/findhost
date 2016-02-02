83c1aa58be02e2c3b7efaa514b5a6ae7b20d7352
Andrew M. Farrell <amfarrell@mit.edu>
2016-02-02 13:30:24 -0600

    add logging

--- src/pickhost/pickhost/settings.py
+++ src/pickhost/pickhost/settings.py
@@ -120,3 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.environ.get('LOG_LEVEL', 'INFO'),
+        },
+    },
+}
