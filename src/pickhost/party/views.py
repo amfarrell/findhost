51c3486c4435d7c16d530179d9942c5a93f434a7
Andrew M. Farrell <amfarrell@mit.edu>
2016-02-06 23:43:54 -0600

    Remove dead view and template code.

--- src/pickhost/party/views.py
+++ src/pickhost/party/views.py
@@ -11,7 +11,7 @@ import json
 # Create your views here.
 
 
-def create_party(request):
+def party(request):
     if 'GET' == request.method:
         return render(request, 'party/create_party.html', {
                 'formset': PartyFormSet()
@@ -22,19 +22,9 @@ def create_party(request):
         formset = PartyFormSet(data)
         if not formset.is_valid():
             return JsonResponse({'errors': formset.errors()})
-            return render(request, 'party/create_party.html', {
-                    'formset': formset
-                })
         else:
             party = Party.objects.create()
             for form in formset.forms:
                 form.instance.party = party
                 form.save()
             return JsonResponse({'best_destination': pick_address(party)})
-            return redirect(reverse('show_party', kwargs={'party_uuid': party.uuid}))
-
-def show_party(request, party_uuid):
-    party = Party.objects.get(uuid = party_uuid)
-    return render(request, 'party/show_party.html', {
-        'party': party
-    })
