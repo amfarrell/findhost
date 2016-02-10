d5b202259671c6a6077089a63a4b86dcf60045d7
Andrew M. Farrell <amfarrell@mit.edu>
2016-02-10 17:11:42 -0600

    More code-style fixes.

--- src/pickhost/static/assets/js/store.js
+++ src/pickhost/static/assets/js/store.js
@@ -1,11 +1,10 @@
-import reducer from './reducer'
-import {createStore} from 'redux'
-import {setState, addMember, changeName, changeAddress} from './action_creators'
+import reducer from './reducer';
+import {createStore} from 'redux';
+import {setState, addMember, changeName, changeAddress} from './action_creators';
 
-const store = createStore(reducer)
-store.dispatch(addMember())
-store.dispatch(addMember())
-store.dispatch(addMember())
+const store = createStore(reducer);
+store.dispatch(addMember());
+store.dispatch(addMember());
+store.dispatch(addMember());
 
-
-export default store
+export default store;
