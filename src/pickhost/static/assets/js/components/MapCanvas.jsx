66261ddf92525e947bee9b7c781bbe6211c7f926
Andrew M. Farrell <amfarrell@mit.edu>
2016-02-09 20:36:04 -0600

    Make google map responsive also.

--- src/pickhost/static/assets/js/components/MapCanvas.jsx
+++ src/pickhost/static/assets/js/components/MapCanvas.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import {map} from '../geo';
+import {map, redrawMap} from '../geo';
 
 export const MapCanvas = React.createClass({
   mixins: [PureRenderMixin],
 
   componentDidMount() {
-    /*
-    */
-
+    //Insert the actual google map.
     map('map-canvas');
+
+    /*
+    Force the map size to be responsive with the hammer
+    of javascript.
+    */
+    window.onresize = () => {
+      this.forceUpdate()
+      redrawMap()
+    }
   },
   render: function() {
-    return <div>
-      <div id="map-canvas" style={{width: '500px', height: '400px'}}></div>
+    /*
+    The google map requires an explicitly-defined width
+    or else it does not appear at all. This means it
+    does not respect any of bootstrap's responsive css
+    rules. Therefore, calculate the width when rendering
+    the react element.
+
+    Note that this does not respond to the user re-sizing
+    the window.
+    */
+    const width = Math.min(window.innerWidth-20, 525);
+    const height = width;
+    console.log("the height is "+height);
+    return <div style={{
+      width : ''+width+'px',
+      height : ''+height+'px',
+      position: 'relative',
+    }}>
+      <div id="map-canvas" style={{
+          position: 'absolute',
+          top: 0, right: 0, bottom: 0, left: 0,
+          margin: 0, bottom: 0}}>
+      </div>
     </div>
   }
 });
--- src/pickhost/static/assets/js/geo.js
+++ src/pickhost/static/assets/js/geo.js
@@ -8,11 +8,11 @@ if ('undefined' == typeof(window.google)){
   The npm packages for geocoding are not reliable, therefore, we just put this
   in a script tag.
   */
-  throw "The window object must have the google api object and a function to get the id of the google map div.";
+  throw "The window object must have the google api object defined.";
 }
 const originalMapOptions = {
   zoom: 12,
-//  center: new window.google.maps.LatLng('42.35925','-71.093781'), //Arbitrarily, center on pMIT
+//  center: new window.google.maps.LatLng('42.35925','-71.093781'), //Arbitrarily, center on MIT
   center: new window.google.maps.LatLng('51.524189','-0.106811'), //Arbitrarily, center on London
   mapTypeId: window.google.maps.MapTypeId.ROADMAP
 }
@@ -24,7 +24,7 @@ export const map = function(mapCanvasId){
     root component has been rendered. Therefore, lazily evaluate it so it can be
     created after the component is rendered.
 
-     It is manipulated through side-effects.
+    It is manipulated through side-effects.
 
     Keep it in a singleton here.
   */
@@ -35,7 +35,6 @@ export const map = function(mapCanvasId){
     const transitLayer = new google.maps.TransitLayer();
     transitLayer.setMap(lazy_map);
   }
-  console.log(typeof(lazy_map));
   if ('undefined' === typeof(lazy_map)){
     map_initialize()
   }
@@ -51,6 +50,7 @@ const makeMarker = function(name, position) {
     title: name
   });
 }
+
 const LatLng = function(latlng_string){
   /* Convert urlstring back to google maps LatLng object */
     const latlng = JSON.parse('['+latlng_string+']');
@@ -75,35 +75,64 @@ export function pickMarker(members, address){
   });
 }
 
+export function redrawMap(members){
+  /*
+  Resize and pan the map based on the updated markers.
+  */
+  if ('undefined' === typeof(members)) {
+    members = store.getState().get('members')
+  }
+  const newBounds = new window.google.maps.LatLngBounds();
+  let atLeastOnePoint;
+  /*
+  If only one position is valid, center the map on it,
+  but don't zoom in all the way.
+  */
+  let points = 0;
+  members.forEach((member) => {
+    if ('undefined' !== typeof(member.get('marker'))){
+      atLeastOnePoint = member.get('marker').getPosition();
+      newBounds.extend(member.get('marker').getPosition());
+      points += 1;
+    }
+  });
+  if (0 === points){
+    map().setOptions(originalMapOptions);
+  } else if (1 === points){
+    map().setOptions(originalMapOptions);
+    map().setCenter(atLeastOnePoint);
+  } else {
+    map().fitBounds(newBounds);
+  }
+}
+
 export function applyGeocode(members, address, latlng) {
   /*
     Fill in the latitude and longitude value for all members with the given address.
     Place pins on the map for them and adjust the bounds of the map.
   */
 
-  const newBounds = new window.google.maps.LatLngBounds();
-  let points = 0;
   const newMembers = members.map((member) => {
-
     if (member.get('address') === address) {
       if (null === latlng){
-        //No geocoded latlng passed. Clear the marker.
-        removeMarker(member);
+        // Position now unknown. Clear the Marker.
+        removeMarker(member)
         return member.merge({
           latlng: undefined,
           latlng_dirty: true,
+          marker: undefined
         })
       }
-
-      points += 1;
-      newBounds.extend(LatLng(latlng));
       if (member.get('marker')) {
+        // Update existing marker to new position.
         member.get('marker').setPosition(LatLng(latlng));
         return member.merge({
           latlng: latlng,
           latlng_dirty: false,
+          marker: member.get('marker')
         })
       } else {
+        // Add new marker.
         return member.merge({
           latlng: latlng,
           latlng_dirty: false,
@@ -111,20 +140,16 @@ export function applyGeocode(members, address, latlng) {
         })
       }
     } else {
-      if (member.get('latlng')){
-        points += 1;
-        newBounds.extend(LatLng(member.get('latlng')));
-      }
       return member
     }
   });
-  if (0 === points){
-    map().setOptions(originalMapOptions);
-  } else if (1 < points){
-    map().fitBounds(newBounds);
-  } else {
-    map().setCenter(LatLng(latlng));
-  }
+  redrawMap(newMembers);
+  /*
+    Side effects in a reducer.
+    A temporary evil to work around Google Maps.
+    TODO: replace this with a callback after state
+    is updated.
+  */
   return newMembers;
 }
 
@@ -133,11 +158,11 @@ export function getGeocode(address) {
   geocoder.geocode({address: address}, (data, status) => {
     if (status == google.maps.GeocoderStatus.OK){
       const latlng = data[0].geometry.location.toUrlValue()
-      console.log("got result back from google. geocode of " + address + " is " + latlng)
       store.dispatch(geocodeFinished(address, latlng))
+      // We got the result back successfully. Update data.
     } else {
+      // Geocode returned nothing. Update to remove stale data.
       store.dispatch(geocodeFinished(address, null))
-      console.log("Geocode was unsuccessful becase " + status)
     }
   });
 }
