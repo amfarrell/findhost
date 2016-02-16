0d60f34adfa55601fdb955088b6d896bee3edd6b
Andrew M. Farrell <amfarrell@mit.edu>
2016-02-16 03:31:00 +0000

    Point to correct location for javascript.

--- src/pickhost/pickhost/settings.py
+++ src/pickhost/pickhost/settings.py
@@ -34,7 +34,7 @@ WEBPACK_LOADER = {
 if not DEBUG:
     WEBPACK_LOADER['DEFAULT'].update({
         'BUNDLE_DIR_NAME': 'dist/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json')
+        'STATS_FILE': os.path.join(WEBPACK_BASE, 'webpack-stats-prod.json')
     })
 
 # Quick-start development settings - unsuitable for production
